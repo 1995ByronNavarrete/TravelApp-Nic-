6e6f00dfa3ccddf18180347012cf66b4d3df1117
Byron <navarretebay74@gmail.com>
2020-11-15 17:06:29 -0600

    v1.0

---
+++ css/editar.php
@@ -0,0 +1,6 @@
+<div id="banner1">
+               <a href="#"><img src="img/fritanga-comida-nicaraguense.jpg" class="img-fluid w-100" alt="Responsive image"></a>
+                <div class="col-md-12 con-text">
+                    <h2 class="text-center">RESTAURANTES</h2>
+                </div>
+            </div>
